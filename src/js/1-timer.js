468c6b28f1545e4c57259fedf2e1c91f343e4849
Yulia Fig <fihlevska@gmail.com>
2026-03-15 16:19:30 +0100

    update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,8 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 let userSelectedDate = null;
 const startBtn = document.querySelector("[data-start]");
 const input = document.querySelector("#datetime-picker");
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,5 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 const form = document.querySelector(".form");
 
 form.addEventListener("submit", event => {
